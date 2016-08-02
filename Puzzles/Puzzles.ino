d3a5acea7e0e7779ed5cf17834b7b141fbe556b7
Molly Nicholas <molecule1985@gmail.com>
2016-08-02 06:39:32 -0700

    First draft of puzzles

---
+++ Puzzles/Puzzles.ino
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2015, 2016 Molly Nicholas
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * (subject to the limitations in the disclaimer below) provided that the following conditions are
+ * met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list of conditions
+ * and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+ * and the following disclaimer in the documentation and/or other materials provided with the
+ * distribution.
+ *
+ * Neither the name of Qbadge nor the names of its contributors may be used to
+ * endorse or promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS LICENSE. THIS
+ * SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+//******** Neopixel ***********//
+#include <Adafruit_NeoPixel.h>
+#include <avr/power.h>
+
+#define NEOPIXEL_PIN 4
+#define NUM_LEDS 18
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
+int brightness = 90;
+int delayVal = 50;
+int pulseDelay = 20;
+int DISPLAY_DELAY = 5000;//ms
+
+uint32_t white         = strip.Color(24, 24, 24);
+uint32_t hot_pink      = strip.Color(48, 0, 24);
+uint32_t light_blue    = strip.Color(0, 24, 24);
+uint32_t bright_blue   = strip.Color(10, 24, 50);
+uint32_t leaf_green    = strip.Color(30, 44, 0);
+uint32_t purple        = strip.Color(17, 0, 44);
+uint32_t yellowOrange  = strip.Color(44, 30, 0);
+uint32_t sapphire      = strip.Color(0, 50, 24);
+uint32_t green         = strip.Color(0, 24, 0);
+uint32_t red           = strip.Color(24, 0, 0);
+uint32_t blue          = strip.Color(0, 0, 24);
+uint32_t salmon_pink   = strip.Color(236, 79, 100); 
+uint32_t cream         = strip.Color(246, 216, 180);
+uint32_t lavendar      = strip.Color(174, 113, 208);
+uint32_t deep_purple   = strip.Color(91, 44, 86); 
+uint32_t medium_purple = strip.Color(232, 100, 255);
+uint32_t yellow        = strip.Color(200, 200, 20);
+uint32_t mediumSeaGreen       = strip.Color(12, 36, 23);
+uint32_t mediumSpringGreen    = strip.Color(0, 50, 20);
+uint32_t lightGreen       = strip.Color(10, 50, 10);
+uint32_t honeyDew         = strip.Color(30, 51, 30);
+uint32_t rosyBrown        = strip.Color(38, 35, 35);
+uint32_t thistle          = strip.Color(43, 24, 43);
+uint32_t plum             = strip.Color(44, 20, 44);
+uint32_t mediumOrchid     = strip.Color(44, 10, 44);
+uint32_t mediumPurple     = strip.Color(44, 5, 44);
+uint32_t mediumSlateBlue  = strip.Color(10, 10, 50);
+uint32_t cornFlowerBlue   = strip.Color(5, 10, 50);
+uint32_t skyBlue          = strip.Color(20, 30, 50);
+uint32_t lightSkyBlue     = strip.Color(27, 30, 50);
+uint32_t paleTurquoise    = strip.Color(20, 30, 48);
+uint32_t powderBlue       = strip.Color(25, 35, 50);
+uint32_t aquamarine       = strip.Color(25, 51, 42);
+uint32_t turquoise        = strip.Color(13, 45, 42);
+uint32_t mediumTurquoise  = strip.Color(14, 42, 41);
+uint32_t lime  = strip.Color(0, 100, 0);
+
+//******** IR Receive **********//
+#include <IRremote.h>
+
+// We need to use the 'raw' pin reading methods because timing is very important here 
+// and the digitalRead() procedure is slower!
+#define IRrx_PIN_VECTOR PIND
+#define IRrx 5
+
+#define MAXPULSE    5000  // the maximum pulse we'll listen for - 5 milliseconds 
+#define NUMPULSES    50  // max IR pulse pairs to sample
+#define RESOLUTION     2  // // time between IR measurements
+
+// we will store up to 50 pulse pairs (this is -a lot-)
+uint16_t pulses[NUMPULSES][2]; // pair is high and low pulse
+uint16_t currentpulse = 0; // index for pulses we're storing
+uint32_t irCode = 0;
+
+const uint32_t IR_REMOTE_POWER     = 0x8322A15E;
+const uint32_t IR_REMOTE_SELECT    = 0x8322A659;
+const uint32_t IR_REMOTE_MUTE      = 0x8322AE51;
+const uint32_t IR_REMOTE_V_UP      = 0x8322A25D;
+const uint32_t IR_REMOTE_V_DOWN    = 0x8322A35C;
+const uint32_t IR_REMOTE_MODE      = 0x8322B24D;
+const uint32_t IR_REMOTE_REWIND    = 0x8322A55A;
+const uint32_t IR_REMOTE_PLAY      = 0x8322B04F;
+const uint32_t IR_REMOTE_FORWARD   = 0x8322A45B;
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  // Timer setup
+  //timer0_init(); //1 ms timer
+  timer1_init(100); // varable timer (1000 = 1 second)
+
+  // Neopixel setup
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  ir_loop();
+
+}
+
+
+
