7319fa564157e52a028f4239cdff96966f215a3a
Molly Nicholas <molecule1985@gmail.com>
2016-08-02 19:12:59 -0700

    Remove unused interrupt

--- Puzzles/Interrupts.ino
+++ Puzzles/Interrupts.ino
@@ -47,12 +47,6 @@ void enablePinInterupt(byte pin)
     PCICR  |= bit (digitalPinToPCICRbit(pin)); // enable interrupt for the group
 }
 
-ISR (PCINT0_vect) // handle pin change interrupt for D8 to D13 here
-{
-  //if(digitalRead(buttonPin)) {  button_press(); }
-  //if(digitalRead(switchPin)) { switch_change(); }
-}
-
 // Timer0 compare interrupt service routine
 SIGNAL(TIMER0_COMPA_vect)
 {
